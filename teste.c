94b65ccafd1f0f3719d0a27fd9b37fa6f2d95787
Wellinghton O. <wellinghtonbatistahotmail@gmail.com>
2025-06-19 14:11:28 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <windows.h>
 
 int main() {
 
 	char caracteres[3][10] = { "[", "=", "]" };
-	
-	printf("%s", caracteres[0]);
-	
+
+	int tempo;
+	printf("Por quantos segundos voce deseja esperar: ");
+	scanf(" %d", &tempo);
+
+	for (int i = 0; i <= tempo; i++ ) {
+		if (i == 0) {
+			printf("%s", caracteres[0]);
+		} else if (i < tempo) {
+			printf("%s", caracteres[1]);
+		} else {
+			printf("%s", caracteres[2]);
+		}
+		Sleep(tempo * 100);
+		//system("cls");
+	}
+
+
 	return 0;
 }
