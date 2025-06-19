54f651bf76c60e933e3381528fa254948192c079
Wellinghton O. <wellinghtonbatistahotmail@gmail.com>
2025-06-19 14:14:15 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -8,6 +8,9 @@ int main() {
 	int tempo;
 	printf("Por quantos segundos voce deseja esperar: ");
 	scanf(" %d", &tempo);
+	
+	Sleep(2000);
+	system("cls");
 
 	for (int i = 0; i <= tempo; i++ ) {
 		if (i == 0) {
@@ -18,7 +21,6 @@ int main() {
 			printf("%s", caracteres[2]);
 		}
 		Sleep(tempo * 100);
-		//system("cls");
 	}
 
 
